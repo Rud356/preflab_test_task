a99839dae5ae918dcd863e599622a7bdefb5b17b
Rud356 <r.moroshkin2015@gmail.com>
2025-08-07 14:15:27 +0300

    Task 4 finished

--- task4/task4.py
+++ task4/task4.py
@@ -1,5 +1,7 @@
 import math
+import statistics
 import sys
+from collections import Counter
 from pathlib import Path
 
 if len(sys.argv) != 2:
@@ -22,8 +24,10 @@ with list_path.open(encoding="utf-8") as f:
     while len((read_number := f.readline())) != 0:
         listed_values.append(int(read_number))
 
+listed_values.sort()
 
-def calculate_optimal_middle_value(values: list[int]) -> int:
+
+def calculate_optimal_average_value(values: list[int]) -> int:
     # List probably should come down to single value, and least distance
     # from the biggest number to smallest is average of all numbers
     average_value: float = 0
@@ -48,15 +52,68 @@ def calculate_optimal_middle_value(values: list[int]) -> int:
     else:
         return math.ceil(average_value)
 
+def calculate_min_max_avg_value(values: list[int]) -> int:
+    # Calculating min and max values average assuming the optimal value is between those two
+    average_value: float = 0
+    sub_average_count: int = 0
+    above_average_count: int = 0
 
-def calculate_steps_count(values: list[int], average: int) -> int:
+    for val in values:
+        if val < average_value:
+            sub_average_count += 1
+
+        else:
+            above_average_count += 1
+
+    average_value = (min(values) + max(values)) / len(values)
+
+    # If there are more numbers below average - it makes sense to get closer to them
+    # and round down instead of up
+    if sub_average_count > above_average_count:
+        return math.floor(average_value)
+
+    else:
+        return math.ceil(average_value)
+
+
+def calculate_best_value_by_mean(values: list[int]) -> int:
+    return int(statistics.median(values))
+
+
+def calculate_steps_count_for_average(values: list[int], average: int) -> int:
     return sum(
         # we only do steps in 1 in each direction, so distance to average is modulus of (value - average)
         map(lambda val: abs(val-average), values)
     )
 
 
+def statistics_counter_optimized_steps(values: list[int]) -> int:
+    # Finding most common value to optimize all other to be that number
+    counter: Counter[int] = Counter(values)
+    most_common_number, most_common_count = counter.most_common(1)[0]
+    return most_common_number
 
-best_average: int = calculate_optimal_middle_value(listed_values)
-steps_count: int = calculate_steps_count(listed_values, best_average)
-print(steps_count)
+
+optimized_variants_step_count: list[int] = [
+    calculate_steps_count_for_average(
+        listed_values,
+        # Works for cases when values have bell curve distribution
+        calculate_optimal_average_value(listed_values)
+    ),
+    calculate_steps_count_for_average(
+        listed_values,
+        # Assuming for event distribution
+        calculate_min_max_avg_value(listed_values)
+    ),
+    calculate_steps_count_for_average(
+        listed_values,
+        # Assuming lots of same values are same, and one or two outliers present
+        statistics_counter_optimized_steps(listed_values)
+    ),
+    calculate_steps_count_for_average(
+        listed_values,
+        # Assuming mean value is least affected and is middle of the values list
+        calculate_best_value_by_mean(listed_values)
+    )
+]
+print(min(optimized_variants_step_count))
